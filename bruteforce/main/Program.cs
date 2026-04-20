cd8e24f931fcde6a63d891d9d4af44f117f2856f
Elisey0000 <elisey0000a@gmail.com>
2026-04-20 19:39:39 +0500

    Add files via upload

---
+++ bruteforce/main/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Diagnostics;
+using System.Runtime.InteropServices;
+string archivePath = "secret_file.7z";
+string outputPath = "выходные файлы";
+string letters = "QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm";
+Directory.CreateDirectory(outputPath);
+string sevenZipPath = @"C:\Program Files (x86)\7-Zip\7z.exe";
+for(int password = 0; password <= 9999; password++)
+{
+    string pass = password.ToString("D4");
+    ProcessStartInfo psi = new ProcessStartInfo
+    {
+        FileName = sevenZipPath,
+        Arguments = $"x \"{archivePath}\" -o\"{outputPath}\" -p{pass} -y",
+        RedirectStandardError = true,
+        RedirectStandardOutput = true,
+    };
+    using(Process? process = Process.Start(psi))
+    {
+        process.WaitForExit();
+        string output = process.StandardOutput.ReadToEnd();
+        if(output.Contains("Everything is Ok"))
+        {
+            Console.WriteLine($"Подходит пароль: {pass}, файл извлечён в папку '{outputPath}'");
+            return;
+        }
+        else
+        {
+            Console.WriteLine($"Пароль {pass} не подошёл");
+        }
+    }
+}
+Console.WriteLine("Не удалось подобрать пароль");
